63ecdad16b9303c5e18770e9364388e345eb7ec5
Unknown <rub@geophysik.tu-freiberg.de>
2020-10-15 10:35:30 +0200

    Add example to docstring

--- src/tools.jl
+++ src/tools.jl
@@ -2,6 +2,12 @@
     dircos(incl, decl)
 
 Compute the three cartesian components of the unit vector aligned with a field vector defined by the two angles inclination `incl` and declination `decl`.
+
+# Example
+```julia-repl
+julia> dircos(56.0, 0.0)
+(0.5591929034707468, 0.0, 0.8290375725550417)
+```
 """
 function dircos(incl, decl)
     xincl = incl * pi / 180.0

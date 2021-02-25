5442fab29aeb4931592c027ff6e636d886317160
Ralph-Uwe Börner <rub@geophysik.tu-freiberg.de>
2021-02-25 09:36:36 +0100

    Add more types

--- src/GravMag.jl
+++ src/GravMag.jl
@@ -2,6 +2,32 @@ module GravMag
 
 using LinearAlgebra
 
+# Type declarations
+struct Point{T}
+    x::T
+    y::T
+    z::T
+end
+
+abstract type Datum end
+
+struct ScalarDatum <: Datum
+    p::Point
+    d::Float64
+    datatype::String
+    unit::String
+end
+
+struct VectorDatum <: Datum
+	p::Point
+	d::Array{Float64, 1}
+	datatype::String
+	unit::String
+	function VectorDatum(p, d, datatype="B", unit="nT")
+		new(p, d, datatype, unit)
+	end
+end
+
 include("mprism.jl")
 include("dipole.jl")
 include("tools.jl")
